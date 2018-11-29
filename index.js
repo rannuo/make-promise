102cc100a6271147035927d242d9d32a259dcf0c
Lero <2632119760@qq.com>
2018-11-29 21:52:20 +0800

    refact the repeat part

--- index.js
+++ index.js
@@ -40,64 +40,52 @@ class P {
     }
   }
 
+  _settle(fun, data) {
+    let ret;
+    try {
+      ret = fun(data);
+    } catch (error) {
+      return P.reject(error);
+    }
+    if (ret instanceof P) {
+      return ret;
+    } else {
+      return P.resolve(ret);
+    }
+  }
+
+  _settleFromPending(fun, data, resolve, reject) {
+    let ret;
+    try {
+      ret = fun(data);
+    } catch (error) {
+      return reject(error);
+    }
+    if (ret instanceof P) {
+      ret.then(resolve, reject)
+    } else {
+      resolve(ret);
+    }
+  }
+
   then(fun1, fun2) {
     if (this.state === 'fulfilled') {
-      let ret;
-      try {
-        ret = fun1(this.value);
-      } catch (error) {
-        return P.reject(error);
-      }
-      if (ret instanceof P) {
-        return ret;
-      } else {
-        return P.resolve(ret);
-      }
+      return this._settle(fun1, this.value);
     }
     if (this.state === 'rejected') {
       if(!fun2) return P.reject(this.error);
-      let ret;
-      try {
-        ret = fun2(this.error);
-      } catch (error) {
-        return P.reject(error)     
-      }
-      if (ret instanceof P) {
-        return ret;
-      } else {
-        return P.resolve(ret);
-      }
+      return this._settle(fun2, this.error);
     } 
     if (this.state === 'pending') {
       return new P((resolve, reject) => {
         this.thenFunc = (value) => {
-          let ret;
-          try {
-            ret = fun1(value);
-          } catch (error) {
-            return reject(error);
-          }
-          if (ret instanceof P) {
-            ret.then(resolve, reject)
-          } else {
-            resolve(ret);
-          }
+          this._settleFromPending(fun1, value, resolve, reject);
         }
         this.catchFunc = (err) => {
           if (!fun2) {
             return reject(err);
           }
-          let ret;
-          try {
-            ret = fun2(err);
-          } catch (error) {
-            return reject(error);
-          }
-          if (ret instanceof P) {
-            ret.then(resolve, reject)
-          } else {
-            resolve(ret);
-          }
+          this._settleFromPending(fun2, err, resolve, reject)
         }
       })
     }
@@ -105,17 +93,7 @@ class P {
 
   catch(fun) {
     if (this.state === 'rejected') {
-      let ret;
-      try {
-        ret = fun(this.error);
-      } catch (error) {
-        return P.reject(error);
-      }
-      if (ret instanceof P) {
-        return ret;
-      } else {
-        return P.resolve(ret);
-      }
+      return this._settle(fun, this.error);
     }
     if (this.state === 'fulfilled') {
       return P.resolve(this.value);
@@ -126,17 +104,7 @@ class P {
           resolve(value);
         }
         this.catchFunc = (err) => {
-          let ret;
-          try {
-            ret = fun(err)
-          } catch (error) {
-            return reject(error);
-          }
-          if (ret instanceof P) {
-            ret.then(resolve, reject)
-          } else {
-            resolve(ret);
-          }
+          return this._settleFromPending(fun, err, resolve, reject);
         }
       })
     }
